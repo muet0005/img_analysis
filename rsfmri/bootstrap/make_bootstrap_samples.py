c4c57b42149edd8620d475d83190e6e7fa024dec
Ryan Muetzel <muet0005@gmail.com>
2013-05-20 11:48:50 +0200

    minor changes to make_bootstrap_samples -- set up paths for lisa.

--- rsfmri/bootstrap/make_bootstrap_samples.py
+++ rsfmri/bootstrap/make_bootstrap_samples.py
@@ -1,18 +1,17 @@
 import os as os
-import gen_bs_samples as gbs
+import knicr.fmri.fmri_utils.gen_bootstrap_samples as gbs
 
-DIR = '/Volumes/mdraid1/mr_data_idc/feb2012_datalock/rfmri'
+DIR = '/home/genr/data/rsfmri'
 
 mDIR = os.path.join(DIR, 'melodic_bootstrap_test')
 
-subj_list = os.path.join(DIR, 'bootstrap_samples', 'test_subj_list.txt')
-
+subj_list = os.path.join(DIR, 'bootstrap_samples', 'subj_list.txt')
 
 gbsc = gbs.gen_bootstrap_samples()
 
 gbsc.read_subj_list(subj_list)
 
-gbsc.gen_samples(1, 10)
+gbsc.gen_samples(10, 35)
 
 gbsc.gen_melodic_lisa(DIR, '_Apr2013.feat', mDIR, 'melodic_test')
 
