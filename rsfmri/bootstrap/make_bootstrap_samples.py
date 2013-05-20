cd41c1f946d8a32efb6eea8f02748f6f67098106
Ryan Muetzel <muet0005@gmail.com>
2013-05-20 11:51:56 +0200

    fixed import statement

--- rsfmri/bootstrap/make_bootstrap_samples.py
+++ rsfmri/bootstrap/make_bootstrap_samples.py
@@ -1,5 +1,5 @@
 import os as os
-import knicr.fmri.fmri_utils.gen_bootstrap_samples as gbs
+import knicr.fmri.fmri_utils as kfmri
 
 DIR = '/home/genr/data/rsfmri'
 
@@ -7,11 +7,11 @@ mDIR = os.path.join(DIR, 'melodic_bootstrap_test')
 
 subj_list = os.path.join(DIR, 'bootstrap_samples', 'subj_list.txt')
 
-gbsc = gbs.gen_bootstrap_samples()
+gbs = kfmri.gen_bootstrap_samples()
 
-gbsc.read_subj_list(subj_list)
+gbs.read_subj_list(subj_list)
 
-gbsc.gen_samples(10, 35)
+gbs.gen_samples(10, 35)
 
-gbsc.gen_melodic_lisa(DIR, '_Apr2013.feat', mDIR, 'melodic_test')
+gbs.gen_melodic_lisa(DIR, '_Apr2013.feat', mDIR, 'melodic_test')
 
