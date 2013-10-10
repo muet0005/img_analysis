b2975b8e3a38d2fb7a17ee1a02416c8bc3133121
Ryan Muetzel <muet0005@gmail.com>
2013-10-10 11:11:53 +0200

    no message

--- rsfmri/bootstrap/match_melodic_components.py
+++ rsfmri/bootstrap/match_melodic_components.py
@@ -5,13 +5,14 @@ import numpy as np
 #from scipy.spatial.distance import rogerstanimoto
 #from scipy.stats.stats import pearsonr as pr
 import scipy.stats.stats as spstat
+import nipype.interfaces.fsl as fsl
 
 #specify the inputs
 DIR = '/Volumes/rbraid/mr_data_idc/aug2013_final/rsfmri/melodic_samples_d16_initial'
 oDIR = os.path.join(DIR, 'matched')
 
 templates = ['cerebellum', 'DMN', 'inferior_mid_frontal', 'insula_subcortical', 'left_pf', 'mid_frontal', 'noise_ant_frontal', 'noise_lower_brainstem', 'noise_pons_vessel', 'noise_sinus', 'noise_sup_frontal', 'noise_susceptibility', 'noise_upper_brainstem', 'noise_vent_wm', 'parietal', 'right_pf', 'sensory_motor', 'superior_mid_frontal', 'visual']
-nsamples = 2
+nsamples = 100
 
 #trg map is the IC that we are checking against
 trg_map = os.path.join(DIR, 'n200', 'melodic_IC_sm6.nii.gz')
@@ -61,17 +62,19 @@ def compute_ui(trg_map, src_map):
 			#scc[c_trg, c_src] = pr(trg_data[:,:,:,c_trg].ravel(), src_data[:,:,:,c_src].ravel())[0]
 			#tdis[c_trg, c_src] = rogerstanimoto(trg_mask_pos.ravel(), src_mask_pos.ravel())
 	#make an empty matrix to store the matches
-	matched_components = np.zeros(trg_data.shape[3])
+	matched_components = np.zeros([2, trg_data.shape[3]])
 	#now iterate through each target component, and find the max U/I...this is your match
 	for i in range(0, trg_data.shape[3]):
 		#print 'Target component: ',  i, ' Source Component: ', np.argmax(ui[i]), np.argmax(scc[i]), np.argmin(tdis[i]), np.max(ui[i]), np.max(scc[i]), np.min(tdis[i])
-		print 'Target component: ',  i, ' Source Component: ', np.argmax(ui[i]), np.max(ui[i])
-        matched_components[i] = [np.argmax(ui[i])]
+		#print 'Target component: ',	 i, ' Source Component: ', np.argmax(ui[i]), np.max(ui[i])
+		matched_components[0][i] = np.argmax(ui[i])
+		matched_components[1][i] = np.max(ui[i])
 		#matched_components[i][np.argmax(scc[i])] += 1
 		#matched_components[i][np.argmax(tdis[i])] += 1
-	mode = spstat.mode(matched_components)
-	print 'Selected component:',  mode
-	return mode
+	#print matched_components
+	#mode = spstat.mode(matched_components)
+	#print 'Selected component:',  int(mode[0][0])
+	return matched_components
 
 
 
@@ -79,13 +82,40 @@ def compute_ui(trg_map, src_map):
 
 
 for sample in range(0, nsamples):
+	if sample < 10:
+		str_sample = '000' + str(sample)
+	elif sample < 100:
+		str_sample = '00' + str(sample)
+	elif sample < 1000:
+		str_sample = '0' + str(sample)	
+	else:
+		str_sample = str(sample)
+	matched_components = {}
 	src_map = os.path.join(DIR, 'sample.' + str(sample) + '.melodic_samples_d16_initial', 'melodic_IC.nii.gz')
 	for template in templates:
 		print template
 		trg_map = os.path.join(DIR, 'templates', template + '_merged.nii.gz')
-		matched_components = compute_ui(trg_map, src_map)
+		matched_components[template] = compute_ui(trg_map, src_map)
+	template_matches = {}
+	for template in matched_components:
+		dict_key = int(spstat.mode(matched_components[template][0])[0][0])
+		if not template_matches.has_key(dict_key):
+			template_matches[dict_key] = template
+			print dict_key, template
+		else:
+			max_new = np.max(matched_components[template][1])
+			max_existing = np.max(matched_components[template_matches[spstat.mode(matched_components[template][0])[0][0]]][1])
+			if max_new > max_existing:
+				print 'Dictionary key: ', dict_key, ' was reassigned from ', template_matches[dict_key], ' to: ', template
+				template_matches[dict_key] = template
+				print max_new, max_existing
+	for vol in template_matches.keys():
+		component = template_matches[vol]
+		oFile = os.path.join(DIR, 'matched', component + '_' + str_sample + '.nii.gz')
+		fslroi = fsl.ExtractROI(in_file=src_map, roi_file=oFile, t_min=vol, t_size=1)
+		fslroi.run()
+#fslmerge = fsl.Merge(dimension='t', terminal_output='stream',in_files=infile_list, merged_file=outfile, output_type='NIFTI_GZ)
 
-#run the function on the sample target and source maps defined above
 
 
 
