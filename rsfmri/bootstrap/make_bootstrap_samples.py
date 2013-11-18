1945790d41aa6087b6c283117062ef5ff184f7b1
Ryan Muetzel <muet0005@gmail.com>
2013-11-18 15:43:50 +0100

    no message

--- rsfmri/bootstrap/make_bootstrap_samples.py
+++ rsfmri/bootstrap/make_bootstrap_samples.py
@@ -4,13 +4,13 @@ import knicr.fmri.fmri_utils as kfmri
 DIR = '/home/genr/data/rsfmri'
 
 nSubs = 50
-nSamples = 100
+nSamples = 1000
 startSample = 0
-dirname = 'melodic_samples_d16_n50_s100'
+dirname = 'melodic_samples_d16_n50_s1000'
 
 mDIR = os.path.join(DIR, dirname)
 
-subj_list = os.path.join(mDIR, 'subj_list.txt')
+subj_list = os.path.join(mDIR, 'cbcl_clinical_cutoff_IDC_N486.txt')
 
 gbs = kfmri.gen_bootstrap_samples()
 
--- rsfmri/bootstrap/run_melodic.sh
+++ rsfmri/bootstrap/run_melodic.sh
@@ -2,13 +2,12 @@
 
 sample=${1}
 
-dirname=melodic_component_matching_d20
-sample_sfix=d20
-ndims=20
+sample_sfix=melodic_samples_d16_n50_s1000
+ndims=16
 tr=2.0
 
-src=${HOME}/data/rsfmri/${dirname}/sample.${sample}.${sample_sfix}
-trg=${TMPDIR}/rmuetzel/rsfmri/${dirname}/sample.${sample}.${sample_sfix}
+src=${HOME}/data/rsfmri/${sample_sfix}/sample.${sample}.${sample_sfix}
+trg=${TMPDIR}/rmuetzel/rsfmri/${sample_sfix}/sample.${sample}.${sample_sfix}
 
 if [ ! -d $trg ] ; then
 	mkdir -p $trg
@@ -16,7 +15,7 @@ fi
 
 rsync -rtvz ${src}/ ${trg}/
 
-export FSLDIR=/home/genr/software/fsl/5.0.4
+export FSLDIR=/home/genr/software/fsl/5.0.5
 source $FSLDIR/etc/fslconf/fsl.sh
 
 ${FSLDIR}/bin/melodic --in=${trg}/sample.${sample} --outdir=${trg} --dim=${ndims} --tr=${tr} --approach=concat --nobet --bgthreshold=10 --mask=$FSLDIR/data/custom/knicr130_t1_2mm_brain_mask.nii.gz
