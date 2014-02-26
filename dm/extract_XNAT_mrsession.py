b74198ae50b0379af43dc48611273610623ba683
Ryan Muetzel <muet0005@gmail.com>
2014-02-26 16:18:04 +0100

    no message

--- dm/extract_XNAT_mrsession.py
+++ dm/extract_XNAT_mrsession.py
@@ -2,9 +2,10 @@ import os as os
 import csv as csv
 import copy as copy 
 
-xnatFile = open('/Users/rmuetzel/Desktop/xnat_summary_nov12.csv', 'r')
-oFile = open('/Users/rmuetzel/Desktop/GenR_F9_MRI_nov_12_2013_FORMATTED.csv', 'w')
+xnatFile = open('/Users/rmuetzel/Desktop/xnat_mrsessions_28jan2014.csv', 'r')
+oFile = open('/Users/rmuetzel/Desktop/GenR_F9_MRI_jan_28_2013_FORMATTED.csv', 'w')
 
+print 'Reading in CSV file from XNAT: ', xnatFile
 csv_reader = csv.reader(xnatFile, delimiter=',')
 
 xnat_mrsession_data = []
@@ -37,14 +38,14 @@ for line in xnat_mrsession_data:
 				seq = seq.replace('(1)', '').replace('(2)', '').replace('(3)', '')
 				if not seq_dict.has_key(seq):
 					seq_dict[seq] = 0
-print 'Sequence dictionary: '
+print 'Found the following sequences:'
 print seq_dict
 
 print 'populating r-dictionary with sequence information...'
 for line in xnat_mrsession_data:
 	if not line[0] == 'MR ID':
 		r = line[0]
-		print r
+		print '.',
 		r_dict[r] = copy.deepcopy(seq_dict)
 		scans=line[5].split('  ')
 		rm_ws = True
@@ -72,6 +73,7 @@ for line in xnat_mrsession_data:
 					count = 4
 				r_dict[r][seq2] = count
 
+print 'Found: ', len(seq_dict.keys()), '  Rnumbers in XNAT...'
 csv_data = []
 headers = ['rnumber']
 for i in seq_dict.keys():
@@ -83,6 +85,11 @@ for r in r_dict.keys():
 		tmp.append(r_dict[r][i])
 	csv_data.append(tmp)
 
+print 'Writing out CSV file: ', oFile
 csv_writer = csv.writer(oFile, delimiter=',')
 csv_writer.writerows(csv_data)
 oFile.close()
+
+
+'Finished.....Press any key to close this window.'
+raw_input()
