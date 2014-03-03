dcc0668182bc95722e1f512ac5254123cbeffc3c
Ryan Muetzel <muet0005@gmail.com>
2014-03-03 16:29:42 +0100

    updated fmriutils to have the option to demean design matrix data.

--- dm/extract_XNAT_mrsession.py
+++ dm/extract_XNAT_mrsession.py
@@ -2,8 +2,8 @@ import os as os
 import csv as csv
 import copy as copy 
 
-xnatFile = open('/Users/rmuetzel/Desktop/xnat_mrsessions_28jan2014.csv', 'r')
-oFile = open('/Users/rmuetzel/Desktop/GenR_F9_MRI_jan_28_2013_FORMATTED.csv', 'w')
+xnatFile = open('/Users/rmuetzel/Desktop/xnat_mrsessions_march1.csv', 'r')
+oFile = open('/Users/rmuetzel/Desktop/GenR_F9_MRI_march1_formatted.csv', 'w')
 
 print 'Reading in CSV file from XNAT: ', xnatFile
 csv_reader = csv.reader(xnatFile, delimiter=',')
@@ -38,6 +38,8 @@ for line in xnat_mrsession_data:
 				seq = seq.replace('(1)', '').replace('(2)', '').replace('(3)', '')
 				if not seq_dict.has_key(seq):
 					seq_dict[seq] = 0
+seq_dict['date'] = False
+
 print 'Found the following sequences:'
 print seq_dict
 
@@ -45,8 +47,10 @@ print 'populating r-dictionary with sequence information...'
 for line in xnat_mrsession_data:
 	if not line[0] == 'MR ID':
 		r = line[0]
+		date = line[1]
 		print '.',
 		r_dict[r] = copy.deepcopy(seq_dict)
+		r_dict[r]['date'] = date
 		scans=line[5].split('  ')
 		rm_ws = True
 		while rm_ws:
@@ -73,7 +77,7 @@ for line in xnat_mrsession_data:
 					count = 4
 				r_dict[r][seq2] = count
 
-print 'Found: ', len(seq_dict.keys()), '  Rnumbers in XNAT...'
+print 'Found: ', len(r_dict.keys()), '  Rnumbers in XNAT...'
 csv_data = []
 headers = ['rnumber']
 for i in seq_dict.keys():
@@ -91,5 +95,5 @@ csv_writer.writerows(csv_data)
 oFile.close()
 
 
-'Finished.....Press any key to close this window.'
+print 'Finished.....Press any key to close this window.'
 raw_input()
