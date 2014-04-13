87844f4f3023388bc098ed7a1a89cf4e76d55cf7
Ryan Muetzel <muet0005@gmail.com>
2014-04-13 12:49:05 +0200

    updates for v5.3

--- tracula/mk_tracula_cfg.py
+++ tracula/mk_tracula_cfg.py
@@ -23,9 +23,9 @@ cfg_str = 'set dcmroot = ' + os.path.join(dtroot, subjid)
 cfg.write(cfg_str + '\n')
 cfg_str = 'set dcmlist = (dti_idc_' + subjid + '_2mm.nii.gz)'
 cfg.write(cfg_str + '\n')
-cfg_str = 'set bvalfile = ' + os.path.join(dtroot, subjid + '/dti_idc_' + subjid + '.tracula.bval')
+cfg_str = 'set bvalfile = ' + os.path.join(dtroot, subjid + '/dti_idc_' + subjid + '.bval')
 cfg.write(cfg_str + '\n')
-cfg_str = 'set bvecfile = ' + os.path.join(dtroot, subjid + '/dti_idc_' + subjid + '.tracula.bvec')
+cfg_str = 'set bvecfile = ' + os.path.join(dtroot, subjid + '/dti_idc_' + subjid + '.bvec')
 cfg.write(cfg_str + '\n')
 cfg_str = 'set nb0 = 3'
 cfg.write(cfg_str + '\n')
@@ -43,11 +43,11 @@ cfg_str = 'set doregmni = 1'
 cfg.write(cfg_str + '\n')
 cfg_str = 'set mnitemp = $FSLDIR/data/standard/MNI152_T1_1mm_brain.nii.gz'
 cfg.write(cfg_str + '\n')
-cfg_str = 'set trainfile = /home/genr/software/bitbucket/lisa/tracula/train_lists/trainlist.txt'
-cfg.write(cfg_str + '\n')
-cfg_str = 'set pathlist = (lh.cst_AS rh.cst_AS lh.ilf_AS rh.ilf_AS lh.unc_AS rh.unc_AS fmajor_PP fminor_PP lh.atr_PP rh.atr_PP lh.cab_PP rh.cab_PP lh.ccg_PP rh.ccg_PP lh.slfp_PP rh.slfp_PP lh.slft_PP rh.slft_PP)'
-cfg.write(cfg_str + '\n')
-cfg_str = 'set ncpts = 5'
-cfg.write(cfg_str + '\n')
-
+#in v5.3, some of these options seem to have changed. The defaults are now included as "all", so these are now comented out.
+#cfg_str = 'set trainfile = /home/genr/software/bitbucket/lisa/tracula/train_lists/trainlist.txt'
+#cfg.write(cfg_str + '\n')
+#cfg_str = 'set pathlist = (lh.cst_AS rh.cst_AS lh.ilf_AS rh.ilf_AS lh.unc_AS rh.unc_AS fmajor_PP fminor_PP lh.atr_PP rh.atr_PP lh.cab_PP rh.cab_PP lh.ccg_PP rh.ccg_PP lh.slfp_PP rh.slfp_PP lh.slft_PP rh.slft_PP)'
+#cfg.write(cfg_str + '\n')
+#cfg_str = 'set ncpts = 5'
+#cfg.write(cfg_str + '\n')
 cfg.close()
